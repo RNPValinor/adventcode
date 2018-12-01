61380a94a452b436eb2d1c435adb7fa14136ce77
Rob Prust <robertprust@hotmail.com>
2018-12-01 07:55:33 +0000

    Add Day 1 answer

--- _2018/Days/Day1.cs
+++ _2018/Days/Day1.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using _2018.Utils;
 
 namespace _2018.Days
@@ -8,6 +11,28 @@ namespace _2018.Days
         public void Run()
         {
             ConsoleUtils.WriteColouredLine("Running day 1", ConsoleColor.DarkGreen);
+
+            List<int> steps;
+
+            var fileStream = new FileStream("Days/Inputs/Day1.txt", FileMode.Open);
+
+            using (var reader = new StreamReader(fileStream))
+            {
+                steps = reader.ReadToEnd().Split(Environment.NewLine).Select(int.Parse).ToList();
+            }
+            
+            var seenFrequencies = new HashSet<int>();
+            var frequency = 0;
+            var i = 0;
+
+            while (!seenFrequencies.Contains(frequency))
+            {
+                seenFrequencies.Add(frequency);
+                frequency += steps[i];
+                i = (i + 1) % steps.Count;
+            }
+            
+            ConsoleUtils.WriteColouredLine($"Target frequency is {frequency}", ConsoleColor.Cyan);
         }
     }
 }
