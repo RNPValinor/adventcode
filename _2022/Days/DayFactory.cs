64fcac6be9396efd4247b6ac6c3d0bd19ba871b5
Robert Prust <robert.prust@solidatus.com>
2022-12-05 08:29:35 +0000

    Solve day 4

--- _2022/Days/DayFactory.cs
+++ _2022/Days/DayFactory.cs
@@ -9,6 +9,7 @@ public class DayFactory
             1 => new Day1(),
             2 => new Day2(),
             3 => new Day3(),
+            4 => new Day4(),
             _ => throw new ArgumentOutOfRangeException(nameof(day), $"Invalid or missing day: {day}")
         };
     }
