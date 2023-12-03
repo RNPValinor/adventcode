52748836d4c9c7a11d973cdd0fabeebddfd840e4
RNPValinor <robertprust@hotmail.com>
2023-12-03 09:31:40 +0000

    Solve day 3

--- _2023/src/solvers/Solvers.java
+++ _2023/src/solvers/Solvers.java
@@ -7,6 +7,7 @@ public class Solvers
         return switch (day) {
             case 1 -> new Day1Solver();
             case 2 -> new Day2Solver();
+            case 3 -> new Day3Solver();
             default -> throw new IndexOutOfBoundsException("Day not supported: " + day);
         };
     }
